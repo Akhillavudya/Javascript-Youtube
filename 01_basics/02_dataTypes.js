5a27f982f83910180fa3b427910ec1d66a8387db
Akhillavudya <163847910+Akhillavudya@users.noreply.github.com>
2025-09-01 20:14:51 +0000

    02_datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) gives error // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Akhil")
+
+
+let name = "Akhil"  //string
+let age = 18   //number
+let isLoggedIn = false //boolean
+let state;    //undifined
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+
+console.log(typeof undefined);  //undefined
+console.log(typeof null) // object
--- 01_variables.js
+++
@@ -1,9 +0,0 @@
-const accountID=230122026;
-var accountEmail="dfhhdjfjd@gmail.com";
-let 
-
-
-
-
-//tc39.es
-//developer.mozilla.org
