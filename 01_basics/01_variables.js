26346f8af1eba2ffe0768025ffe1cf1e754a0743
Akhillavudya <163847910+Akhillavudya@users.noreply.github.com>
2025-09-01 20:01:35 +0000

    01_variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,30 @@
+
+
+const accountId=2234
+let accountEmail="akhil4567@gmail.com"
+var accountPassword="akhil45677"
+accountiCity="Hyderabad"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail ="aku4567"
+accountPassword="213232"
+accountCity="Delhi"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+// console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+//tc39.es
+//developer.mozilla.org
---
+++ 01_variables.js
@@ -0,0 +1,9 @@
+const accountID=230122026;
+var accountEmail="dfhhdjfjd@gmail.com";
+let 
+
+
+
+
+//tc39.es
+//developer.mozilla.org
