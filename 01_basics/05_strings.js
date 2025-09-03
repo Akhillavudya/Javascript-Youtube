25a35df020d5e3e6e01c07a5adc8eb0765fcdd66
Akhillavudya <163847910+Akhillavudya@users.noreply.github.com>
2025-09-03 18:06:38 +0000

    05_strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,38 @@
+const name="Akhil"
+const repoCount= 50
+
+// console.log(name + repoCount + "loveU")   --not an good code
+
+// console.log(`my name is ${name} and the repo count ${repoCount} and value is ${"good"}`)
+
+const gameName = new String('Akhil-Aku-com')
+
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('i'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+const newStringOne = "   Akhil    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
