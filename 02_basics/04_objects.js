f224d7c391c4adc0d1974bc2c2048f3b421f478b
Akhillavudya <163847910+Akhillavudya@users.noreply.github.com>
2025-09-26 07:13:28 +0000

    arra

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,81 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Akhil",
+            lastname: "Lavudya"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }  //not correct combine
+// const obj3 = Object.assign({}, obj1, obj2, obj4) // combine in one object
+
+const obj3 = {...obj1, ...obj2}//same as before
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+
+users[1].email
+console.log(tinderUser);
+
+// console.log(Object.keys(tinderUser)); //gives only keys
+// console.log(Object.values(tinderUser));//gives only values
+// console.log(Object.entries(tinderUser));// give in array in pairs
+
+console.log(tinderUser.hasOwnProperty('isLoggedIn'));//true
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+// // [
+//     {},
+//     {},
+//     {}
+// // ]
